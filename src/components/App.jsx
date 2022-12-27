bdda680308d0ed5b7a01020b777cf5c39f574f62
Aleksey <avralgroup@gmail.com>
2022-12-27 10:52:02 +0200

    corrected

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,12 @@
 import { Profile } from "./Profile/Profile";
-import { Statistics } from "./Statistics/Statistics";
-import { FriendList } from "./FriendList/FriendList";
-import { TransactionHistory } from "./TransactionHistory/TransactionHistory";
+// import { Statistics } from "./Statistics/Statistics";
+// import { FriendList } from "./FriendList/FriendList";
+// import { TransactionHistory } from "./TransactionHistory/TransactionHistory";
 
 import user from "../assets/user.json";
-import data from "../assets/data.json";
-import friends from "../assets/friends.json";
-import transactions from "../assets/transactions.json";
+// import data from "../assets/data.json";
+// import friends from "../assets/friends.json";
+// import transactions from "../assets/transactions.json";
 
 export const App = () => {
   return (
@@ -30,7 +30,7 @@ export const App = () => {
         stats={user.stats}
       />
 
-      <Statistics
+      {/* <Statistics
         title="Upload stats"
         stats={data}
       />
@@ -41,7 +41,7 @@ export const App = () => {
 
       <TransactionHistory
         items={transactions}
-      />
+      /> */}
 
     </div>
   );
--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -1,18 +1,18 @@
-import PropTypes from 'prop-types';
-import css from './FriendList.module.css';
+// import PropTypes from 'prop-types';
+// import css from './FriendList.module.css';
 
-export const FriendListItem = ({ avatar, name, isOnline }) => {
-  return (
-    <li className={css.listItem}>
-      <span className={isOnline ? css.online : css.offline}></span>
-      <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
-      <p className={css.name}>{name}</p>
-    </li>
-  )
-}
+// export const FriendListItem = ({ avatar, name, isOnline }) => {
+//   return (
+//     <li className={css.listItem}>
+//       <span className={isOnline ? css.online : css.offline}></span>
+//       <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
+//       <p className={css.name}>{name}</p>
+//     </li>
+//   )
+// }
 
-FriendListItem.propTypes = {
-  avatar: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-  isOnline: PropTypes.bool.isRequired,
-}
+// FriendListItem.propTypes = {
+//   avatar: PropTypes.string.isRequired,
+//   name: PropTypes.string.isRequired,
+//   isOnline: PropTypes.bool.isRequired,
+// }
